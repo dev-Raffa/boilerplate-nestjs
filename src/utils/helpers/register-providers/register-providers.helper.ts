291228f3f1ecd90f7ab7d0030948554f9c163925
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-28 16:17:20 -0300

    refatc(helpers): add type registerProvidersOptions

--- src/utils/helpers/register-providers/register-providers.helper.ts
+++ src/utils/helpers/register-providers/register-providers.helper.ts
@@ -1,21 +1,28 @@
 import { Provider, Type } from '@nestjs/common';
 import { DataSource, EntityTarget, ObjectLiteral } from 'typeorm';
 
+export type registerProvidersOptions = {
+  entity: EntityTarget<ObjectLiteral>;
+  service: Type<any>;
+  others?: Provider[];
+};
+
 export const registerProviders = (
-  entity: EntityTarget<ObjectLiteral>,
-  service: Type<any>,
-  others?: Provider[]
+  options: registerProvidersOptions
 ): Provider[] => {
   const baseProviders: Provider[] = [
     {
       provide: 'REPOSITORY',
-      useFactory: (datasource: DataSource) => datasource.getRepository(entity),
+      useFactory: (datasource: DataSource) =>
+        datasource.getRepository(options.entity),
       inject: ['DATA_SOURCE']
     },
     {
       provide: 'SERVICE',
-      useClass: service
+      useClass: options.service
     }
   ];
-  return others ? [...baseProviders, ...others] : [...baseProviders];
+  return options.others
+    ? [...baseProviders, ...options.others]
+    : [...baseProviders];
 };
