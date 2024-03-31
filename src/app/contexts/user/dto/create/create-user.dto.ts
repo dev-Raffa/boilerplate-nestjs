0c32ca04e4564dccb18f357219949c5dce52237e
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-31 03:50:48 -0300

    refact(DTO): add minValue & minLenth  decorators in the createUserDTO.

--- src/app/contexts/user/dto/create/create-user.dto.ts
+++ src/app/contexts/user/dto/create/create-user.dto.ts
@@ -1,10 +1,17 @@
+import { minLength } from '../../../../../utils/decorators/minLength/minLength.decorator';
 import { isEmail } from '../../../../../utils/decorators/isEmail/isEmail.decorator';
 import { IUser } from '../../model/user.model';
+import { minValue } from '../../../../../utils/decorators/minValue/minValue.decorator';
 
 export class CreateUserDTO implements Omit<IUser, 'id'> {
+  @minLength({ min: 3 })
   name: string;
+
+  @minValue({ min: 16 })
   age: number;
-  @isEmail() email: string;
+
+  @isEmail()
+  email: string;
 
   constructor(args: Omit<IUser, 'id'>) {
     this.name = args.name;
