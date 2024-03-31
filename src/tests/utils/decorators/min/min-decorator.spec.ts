fc4593e19674660f17a04b095aa3c665bceeba0d
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-30 22:49:31 -0300

    test(decorator): add min decorator tests

---
+++ src/tests/utils/decorators/min/min-decorator.spec.ts
@@ -0,0 +1,21 @@
+import { NumberValidator } from '../../../../utils/validators/number/number.validator';
+import { Validate } from '../../../../utils/decorators/Validate/validate.decorator';
+import { min } from '../../../../utils/decorators/min/min.decorator';
+
+jest.mock('../../../../utils/decorators/Validate/validate.decorator', () => ({
+  Validate: jest.fn()
+}));
+
+describe.skip('min decorator', () => {
+  it('should call the Validate method with Validator & "the value should be greater 4"', () => {
+    const minDecorator = min({ min: 5 });
+    minDecorator;
+
+    expect(Validate).toHaveBeenCalledWith(new NumberValidator(), {
+      errorMsg: 'the value should be greater 4',
+      options: {
+        min: 5
+      }
+    });
+  });
+});
