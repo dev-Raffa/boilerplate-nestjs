191ccefacf5514fbd97b2799eba962a31a872752
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-30 22:25:29 -0300

    test(decorator): add minLength decorator tests

---
+++ src/tests/utils/decorators/minLength/minLength-decorator.spec.ts
@@ -0,0 +1,21 @@
+import { Validate } from '../../../../utils/decorators/Validate/validate.decorator';
+import { minLength } from '../../../../utils/decorators/minLength/minLength.decorator';
+import { LengthValidator } from '../../../../utils/validators/length/length.validator';
+
+jest.mock('../../../../utils/decorators/Validate/validate.decorator', () => ({
+  Validate: jest.fn()
+}));
+
+describe.skip('minLength decorator', () => {
+  it('should call the Validate method with lengthValidator & "the value.length should be greater 4"', () => {
+    const minLengthDecorator = minLength({ min: 5 });
+    minLengthDecorator;
+
+    expect(Validate).toHaveBeenCalledWith(new LengthValidator(), {
+      errorMsg: 'the value.length should be greater 4',
+      options: {
+        min: 5
+      }
+    });
+  });
+});
