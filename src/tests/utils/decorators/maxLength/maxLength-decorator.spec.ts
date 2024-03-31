13655a987ccf7f944e648ebf5e7d0bf9dc9d6cf0
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-30 22:33:46 -0300

    test(decorator): add maxLength decorator tests

---
+++ src/tests/utils/decorators/maxLength/maxLength-decorator.spec.ts
@@ -0,0 +1,21 @@
+import { Validate } from '../../../../utils/decorators/Validate/validate.decorator';
+import { maxLength } from '../../../../utils/decorators/maxLength/maxLength.decorator';
+import { LengthValidator } from '../../../../utils/validators/length/length.validator';
+
+jest.mock('../../../../utils/decorators/Validate/validate.decorator', () => ({
+  Validate: jest.fn()
+}));
+
+describe.skip('maxLength decorator', () => {
+  it('should call the Validate method with lengthValidator & "the value.length should be lower 10"', () => {
+    const maxLengthDecorator = maxLength({ max: 10 });
+    maxLengthDecorator;
+
+    expect(Validate).toHaveBeenCalledWith(new LengthValidator(), {
+      errorMsg: 'the value.length should be lower 11',
+      options: {
+        max: 10
+      }
+    });
+  });
+});
