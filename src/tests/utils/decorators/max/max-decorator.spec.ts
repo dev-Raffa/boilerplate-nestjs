b04bc97ea4bd9604f1a5b53358a5510fc885e197
Rafael da Conceição <mnraffaell@gmail.com>
2024-03-30 23:02:42 -0300

    test(decorator): add max decorator tests

---
+++ src/tests/utils/decorators/max/max-decorator.spec.ts
@@ -0,0 +1,21 @@
+import { NumberValidator } from '../../../../utils/validators/number/number.validator';
+import { Validate } from '../../../../utils/decorators/Validate/validate.decorator';
+import { max } from '../../../../utils/decorators/max/max.decorator';
+
+jest.mock('../../../../utils/decorators/Validate/validate.decorator', () => ({
+  Validate: jest.fn()
+}));
+
+describe.skip('max decorator', () => {
+  it('should call the Validate method with numberValidator & "the value should be lower 10"', () => {
+    const maxDecorator = max({ max: 10 });
+    maxDecorator;
+
+    expect(Validate).toHaveBeenCalledWith(new NumberValidator(), {
+      errorMsg: 'the value should be lower 11',
+      options: {
+        max: 10
+      }
+    });
+  });
+});
