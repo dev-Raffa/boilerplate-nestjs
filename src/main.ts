2f8a18e2f6ee1485e2058e349ba01ae269925132
dev-Raffa <mnraffaell@gmail.com>
2024-04-21 11:29:29 -0300

    refact(CORS): add access-key to allowedHeaders

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: ['http://localhost:3000'],
-      allowedHeaders: ['Content-Type', 'Accept'],
+      allowedHeaders: ['Content-Type', 'Accept', 'access-key'],
       methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
       credentials: true,
       optionsSuccessStatus: 204,
